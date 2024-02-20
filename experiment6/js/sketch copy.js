83e494693cbe1a29181b3df5399e8ad7eefb3352
Aaron-Vuong <81384460+Aaron-Vuong@users.noreply.github.com>
2024-02-20 00:20:38 -0800

    p6

--- experiment6/js/sketch copy.js
+++ experiment6/js/sketch copy.js
@@ -22,15 +22,14 @@ let sample_text;
 function setup() {
     // place our canvas, making it fit our container
     canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height() - 100);
+    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
     canvas.parent("canvas-container");
     // resize canvas is the page is resized
     $(window).resize(function() {
         console.log("Resizing...");
-        resizeCanvas(canvasContainer.width(), canvasContainer.height() - 100);
+        resizeCanvas(canvasContainer.width(), canvasContainer.height());
     });
     input=createInput();
-    input.parent("canvas-container")
     sample_text = str(adjectives[int(random(adjectives.length))]) + " " + str(nouns[int(random(nouns.length))]) + " " + str(verbs[int(random(verbs.length))])
     
     var centerHorz = windowWidth / 2;
